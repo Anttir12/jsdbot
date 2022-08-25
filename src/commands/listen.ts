1f293c346b37bc821c3d01e2b8493a50843b7b7b
Antti <antti.ruohisto@gmail.com>
2022-08-25 22:00:48 +0300

    remove listen parameter. Also return token to user

--- src/commands/listen.ts
+++ src/commands/listen.ts
@@ -2,105 +2,93 @@ import 'dotenv/config';
 import {ChatInputCommandInteraction, GuildMember, SlashCommandBuilder} from 'discord.js';
 import {AudioReceiveStream, EndBehaviorType, getVoiceConnection} from '@discordjs/voice';
 import * as prism from 'prism-media';
-import { Command } from "./command";
+import {Command} from "./command";
 import * as websocket from 'websocket-stream';
 import {getWsToken} from "../dbot-client";
 
 interface AudioListener {
-	stream: AudioReceiveStream
-	webSocket: websocket.WebSocketDuplex
+    stream: AudioReceiveStream
+    webSocket: websocket.WebSocketDuplex
 }
+
 const currentlyListening: Record<string, AudioListener> = {};
 
 const command: Command = {
-	data: new SlashCommandBuilder()
-		.setName('listen')
-		.addBooleanOption(option =>
-			option.setName('stop')
-				.setDescription('Stop listening')
-				.setRequired(false))
-		.setDescription('test listen'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		const stop = interaction.options.getBoolean('stop', false)
-			const member = interaction.member as GuildMember
-		const voiceChannel = member.voice?.channel;
-		if (voiceChannel) {
-			const userId = member.id.toString();
-			if(userId in currentlyListening) {
-				if(stop){
-					if(killListening(userId)) {
-						await interaction.reply({ content: 'No longer listening!', ephemeral: true });
-					} else {
-						await interaction.reply({ content: 'Couldnt stop listening. Maybe I wasn\'t listening ' +
-								'you in the first place?', ephemeral: true });
-					}
-				}else{
-					await interaction.reply({ content: 'Already listening you!', ephemeral: true });
-				}
-			} else {
-				const wsToken = await getWsToken();
-				if(wsToken) {
-					const connection = getVoiceConnection(voiceChannel.guild.id);
-					const opusStream = connection.receiver.subscribe(userId, {
-						end: {
-							behavior: EndBehaviorType.Manual,
-						},
-					});
-					const ws = websocket(`${process.env.DBOT_LISTEN_STREAM}?wsToken=${wsToken}`)
+    data: new SlashCommandBuilder()
+        .setName('listen')
+        .setDescription('Starts to listen and analyzing your speech'),
+    async execute(interaction: ChatInputCommandInteraction) {
+        const member = interaction.member as GuildMember
+        const userId = member.id.toString();
+        if (userId in currentlyListening) {
+            killListening(userId);
+            await interaction.reply({content: 'No longer listening!', ephemeral: true});
+        } else {
+            const voiceChannel = member.voice?.channel;
+            if (voiceChannel) {
+                const userId = member.id.toString();
+                const wsToken = await getWsToken();
+                if (wsToken) {
+                    const connection = getVoiceConnection(voiceChannel.guild.id);
+                    const opusStream = connection.receiver.subscribe(userId, {
+                        end: {
+                            behavior: EndBehaviorType.Manual,
+                        },
+                    });
+                    const ws = websocket(`${process.env.DBOT_LISTEN_STREAM}?wsToken=${wsToken}`)
 
+                    currentlyListening[userId] = {stream: opusStream, webSocket: ws};
+                    const oggStream = new prism.opus.OggLogicalBitstream({
+                        opusHead: new prism.opus.OpusHead({
+                            channelCount: 2,
+                            sampleRate: 48000,
+                        }),
+                        pageSizeControl: {
+                            maxPackets: 10,
+                        },
+                    });
 
-					currentlyListening[userId] = {stream: opusStream, webSocket: ws};
+                    ws.on('open', () => {
+                        console.log("Opened WebSocket connection!")
+                        console.log("Starting to listen!")
+                    })
 
-					const oggStream = new prism.opus.OggLogicalBitstream({
-						opusHead: new prism.opus.OpusHead({
-							channelCount: 2,
-							sampleRate: 48000,
-						}),
-						pageSizeControl: {
-							maxPackets: 10,
-						},
-					});
+                    ws.on('error', (err) => {
+                        console.log("Websocket error!")
+                        console.log(err)
+                        killListening(userId)
+                    })
 
-					ws.on('open', () => {
-						console.log("Opened WebSocket connection!")
-						console.log("Starting to listen!")
-					})
+                    ws.on('closed', () => {
+                        console.log("Closed WebSocket connection!")
+                        killListening(userId)
+                    })
 
-					ws.on('error', (err) => {
-						console.log("Websocket error!")
-						console.log(err)
-						killListening(userId)
-					})
+                    opusStream.pipe(oggStream).pipe(ws);
 
-					ws.on('closed', () => {
-						console.log("Closed WebSocket connection!")
-						killListening(userId)
-					})
-
-					opusStream.pipe(oggStream).pipe(ws);
-
-					await interaction.reply({content: 'Listening you!', ephemeral: true});
-				} else {
-					await interaction.reply({content: 'Failed to get WS token', ephemeral: true})
-				}
-			}
-		}
-		else {
-			await interaction.reply({ content: 'Your are not in a voice channel!', ephemeral: true});
-		}
-	},
+                    await interaction.reply({
+                        content: `Listening you! your stt_token: ${wsToken}`,
+                        ephemeral: true});
+                } else {
+                    await interaction.reply({content: 'Failed to get WS token', ephemeral: true})
+                }
+            } else {
+                await interaction.reply({content: 'Your are not in a voice channel!', ephemeral: true});
+            }
+        }
+    },
 };
 
 const killListening = (userId: string) => {
-	const audioListener = currentlyListening[userId]
-	if(audioListener){
-		console.log("Killing listening things")
-		audioListener.stream.destroy()
-		audioListener.webSocket.destroy()
-		delete currentlyListening[userId]
-		return true
-	}
-	return false
+    const audioListener = currentlyListening[userId]
+    if (audioListener) {
+        console.log("Killing listening things")
+        audioListener.stream.destroy()
+        audioListener.webSocket.destroy()
+        delete currentlyListening[userId]
+        return true
+    }
+    return false
 }
 
-export { command };
+export {command};
