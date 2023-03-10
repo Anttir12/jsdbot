f4663f27b000308d4dfa159fd95bae7ed9e910b6
Anttir12 <antti.ruohisto@gmail.com>
2023-03-10 19:14:12 +0200

    Workaround to voice bug

--- src/voice.ts
+++ src/voice.ts
@@ -76,6 +76,16 @@ const joinVoice = (voiceChannel: VoiceChannel|VoiceBasedChannel) => {
         selfDeaf: false,
     });
     connection.subscribe(player);
+
+    const networkStateChangeHandler = (oldNetworkState: any, newNetworkState: any) => {
+      const newUdp = Reflect.get(newNetworkState, 'udp');
+      clearInterval(newUdp?.keepAliveInterval);
+    }
+    connection.on('stateChange', (oldState, newState) => {
+      Reflect.get(oldState, 'networking')?.off('stateChange', networkStateChangeHandler);
+      Reflect.get(newState, 'networking')?.on('stateChange', networkStateChangeHandler);
+    });
+
 }
 
 export { player, clearQueue, showQueue, joinVoice };
