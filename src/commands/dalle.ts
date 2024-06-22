a027af1c3cdef718a951b770d8d26b11c598460b
antti <antti.ruohisto@gmail.com>
2024-06-22 23:10:54 +0300

    Update packages and use dalle3

--- src/commands/dalle.ts
+++ src/commands/dalle.ts
@@ -1,65 +1,82 @@
 import {
-	ChatInputCommandInteraction,
-	EmbedBuilder,
-	SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
 } from 'discord.js';
 import { Command } from './command';
-import {Configuration, CreateImageRequestSizeEnum, ImagesResponse, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 import 'dotenv/config';
 
+type sizeChoices =
+  | '256x256'
+  | '512x512'
+  | '1024x1024'
+  | '1792x1024'
+  | '1024x1792';
 
 const command: Command = {
-	data: new SlashCommandBuilder().setName('dalle')
-		.addStringOption(option =>
-            option.setName("prompt")
-                  .setDescription("The prompt to generate the image from")
-                  .setRequired(true))
-        .addStringOption(option =>
-			option.setName("size")
-				.setDescription("Size of the generated images")
-				.setRequired(true)
-				.addChoices(
-        	    {name: "256", value:"256x256"},
-				{name: "512", value: "512x512"},
-    	        {name: "1024", value: "1024x1024"}
-				))
-		.addIntegerOption(option =>
-                  option.setDescription("How many images to create (1-10)")
-					    .setName("count")
-                        .setMaxValue(10)
-                        .setMinValue(1)
-                        .setRequired(false))
-		.setDescription('Create an image with Dalle!'),
+  data: new SlashCommandBuilder()
+    .setName('dalle')
+    .addStringOption((option) =>
+      option
+        .setName('prompt')
+        .setDescription('The prompt to generate the image from')
+        .setRequired(true),
+    )
+    .addStringOption((option) =>
+      option
+        .setName('size')
+        .setDescription('Size of the generated images')
+        .setRequired(true)
+        .addChoices(
+          { name: '256x256', value: '256x256' },
+          { name: '512x512', value: '512x512' },
+          { name: '1024x1024', value: '1024x1024' },
+          { name: '1792x1024', value: '1792x1024' },
+          { name: '1024x1792', value: '1024x1792' },
+        ),
+    )
+    .setDescription('Create an image with Dalle!'),
 
-	async execute(interaction: ChatInputCommandInteraction) {
-		const prompt = interaction.options.getString("prompt");
-		const size = interaction.options.getString("size");
-		const count = interaction.options.getInteger("count") ?? 1;
+  async execute(interaction: ChatInputCommandInteraction) {
+    const prompt = interaction.options.getString('prompt');
+    const size = interaction.options.getString('size');
 
-		const configuration = new Configuration({
-			apiKey: process.env.OPENAI_API_KEY,
-		})
-		const openai = new OpenAIApi(configuration)
-		const replyMessage = count > 1 ? "Generated images" : "Generated image"
-		const reply = await interaction.reply({content: "Generating images...", ephemeral: false})
-		openai.createImage({
-			prompt: prompt,
-			n:  count,
-			size: size as CreateImageRequestSizeEnum
-		}).then(async (response) => {
-			const images = response.data as ImagesResponse;
-			let i = 1;
-			const urls = images.data.map(img => {
-				return new EmbedBuilder().setTitle(`image #${i++}`).setImage(img.url)
-			});
-			await reply.edit({content: `${replyMessage} with prompt "${prompt}"`, embeds: urls});
-		}).catch(async (reason) => {
-			let replyReason = `Request failed (${reason?.response?.status})`;
-			let errorMessage = reason?.response?.data?.error?.message;
-			let errorLine2 = errorMessage ? `\n${errorMessage}` : "";
-			await reply.edit({content: `${replyReason}${errorLine2}`});
-		});
-	},
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    const replyMessage = 'Generated image';
+    const reply = await interaction.reply({
+      content: 'Generating image...',
+      ephemeral: false,
+    });
+    openai.images
+      .generate({
+        model: 'dall-e-3',
+        prompt: prompt,
+        n: 1,
+        size: size as sizeChoices,
+      })
+      .then(async (response) => {
+        const images = response.data;
+        let i = 1;
+        const urls = images.map((img) => {
+          return new EmbedBuilder().setTitle(`image #${i++}`).setImage(img.url);
+        });
+        await reply.edit({
+          content: `${replyMessage} with prompt "${prompt}"`,
+          embeds: urls,
+        });
+      })
+      .catch(async (reason) => {
+        console.log('Failed');
+        console.log(reason);
+        let replyReason = `Request failed (${reason?.error?.message})`;
+        let errorMessage = reason?.response?.data?.error?.message;
+        let errorLine2 = errorMessage ? `\n${errorMessage}` : '';
+        await reply.edit({ content: `${replyReason}${errorLine2}` });
+      });
+  },
 };
 
 export { command };
